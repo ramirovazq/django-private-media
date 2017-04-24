3745d09e08b63c1cc2d680b0a134af7ea15c92fb
ramirovazq <ramirovazquez95@gmail.com>
2017-04-24 18:10:00 -0500

    Update urls.py
    Django version 1.10.1

--- private_media/urls.py
+++ private_media/urls.py
@@ -1,6 +1,7 @@
-from django.conf.urls import patterns, url
+from django.conf.urls import url
 from django.conf import settings
+from . import views
 
-urlpatterns = patterns('private_media.views',
-    url(r'^{0}(?P<path>.*)$'.format(settings.PRIVATE_MEDIA_URL.lstrip('/')), 'serve_private_file',),
-)
+urlpatterns = [
+    url(r'^{0}(?P<path>.*)$'.format(settings.PRIVATE_MEDIA_URL.lstrip('/')), views.serve_private_file),
+]
